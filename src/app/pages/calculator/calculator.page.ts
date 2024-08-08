e915a197a4618e55213009f0eb43d5fcac302d8a
Miguel Ferreira <miguelsf1@sapo.pt>
2024-08-08 22:26:50 +0100

    finished UI

--- src/app/pages/calculator/calculator.page.ts
+++ src/app/pages/calculator/calculator.page.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, ToastController, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.page.html',
   styleUrls: ['./calculator.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule]
+  imports: [IonContent, CommonModule, IonButton]
 })
-export class CalculatorPage implements OnInit {
+export class CalculatorPage {
+  private toastCtrl = inject(ToastController);
   public results: any = [];
   public expression: any = "";
 
   constructor() { }
 
-  ngOnInit() {}
-
   private checkExpr() {
-    // use regex
-    return true;
+    const regex = /(\d+(\.\d+)?|\(\d+(\.\d+)?(?:[/*+\-]\d+(\.\d+)?)*\))(?:[/*+\-](\d+(\.\d+)?|\(\d+(\.\d+)?(?:[/*+\-]\d+(\.\d+)?)*\))*)+/g; // needs work
+    return this.expression.match(regex) !== null;
   }
 
   public calcRes() {
@@ -28,7 +27,7 @@ export class CalculatorPage implements OnInit {
       this.results.unshift({ expr: this.expression, result: res });
       this.expression = res + "";
     } else {
-      // send toast
+      this.presentInvalidExprToast();
     }
   }
 
@@ -40,5 +39,16 @@ export class CalculatorPage implements OnInit {
     } else {
       this.expression += char;
     }
+  }
+
+  private async presentInvalidExprToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Invalid Expression',
+      duration: 3000,
+      position: 'top',
+      color: 'primary'
+    });
+
+    await toast.present();
   }
 }
