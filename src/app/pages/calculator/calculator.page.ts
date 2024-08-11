6e2595e039b6df5a9ef55e4ae82a0f7a5f1abacb
Miguel Ferreira <miguelsf1@sapo.pt>
2024-08-11 22:45:49 +0100

    improved math expr logic

--- src/app/pages/calculator/calculator.page.ts
+++ src/app/pages/calculator/calculator.page.ts
@@ -17,8 +17,65 @@ export class CalculatorPage {
   constructor() { }
 
   private checkExpr() {
-    const regex = /(\d+(\.\d+)?|\(\d+(\.\d+)?(?:[/*+\-]\d+(\.\d+)?)*\))(?:[/*+\-](\d+(\.\d+)?|\(\d+(\.\d+)?(?:[/*+\-]\d+(\.\d+)?)*\))*)+/g; // needs work
-    return this.expression.match(regex) !== null;
+    let openParen = 0;
+    let lastOper = false;
+    let lastNum = false;
+    let lastDecim = false;
+    let lastOpenPar = false;
+    let lastClosePar = false;
+
+    for (let i = 0; i < this.expression.length; i++) {
+      if (this.isDigit(this.expression[i])) {
+        if (lastClosePar) {
+          return false;
+        }
+        lastNum = true;
+        lastOper = false;
+        lastOpenPar = false;
+      } else if (this.expression[i] === '.') {
+        if (!lastNum || lastDecim) {
+          return false;
+        }
+        lastDecim = true;
+        lastNum = false;
+      } else if (this.expression[i] === '(') {
+        if (!lastOper) {
+          return false;
+        }
+        openParen++;
+        lastOpenPar = true;
+        lastOper = false;
+      } else if (this.expression[i] === ')') {
+        if (!lastNum) {
+          return false;
+        }
+        openParen--;
+        lastClosePar = true;
+        lastNum = false;
+      } else if (this.isOperator(this.expression[i])) {
+        if (!lastNum && !lastClosePar) {
+          return false;
+        }
+        lastOper = true;
+        lastNum = false;
+        lastClosePar = false;
+        lastDecim = false;
+      }
+
+      if (openParen < 0) {
+        return false;
+      }
+    }
+
+    return openParen === 0 && this.expression[this.expression.length - 1] !== '.' && !this.isOperator(this.expression[this.expression.length - 1]);
+  }
+
+  private isDigit(c: string) {
+    return c >= '0' && c <= '9';
+  }
+
+  private isOperator(c: string) {
+    return c === '+' || c === '-' || c === '*' || c === '/'; 
   }
 
   public calcRes() {
